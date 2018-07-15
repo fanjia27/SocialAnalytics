9e2d72f7ffbb604a4c5b42fc3edb2ddba268ffcd
jiafan <fanjia27@gmail.com>
2018-07-14 21:36:40 -0700

    social_analytics_hw

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "V7dUAUC4MaK5JMzwDAXYwF3en"
+consumer_secret = "BTwHTCsnct2QNO3yeQqHfF8k3KzYRtoUXhXxshS1FTmhDF5tTV"
+access_token = "1010574587283718150-WWLNZ2R4zr8xwXHF84wlcJqXSjE6fI"
+access_token_secret = "S6JGxEGB6jZs1P98QOlMvXn3A8cBUpTCcHVMofgXv8QlH"
